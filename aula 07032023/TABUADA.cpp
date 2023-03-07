16b4b52702ed3cc9ff4b674e7518a1afdbe1cfdb
Raposo <92121267+at-raposo@users.noreply.github.com>
2023-03-07 20:34:25 -0300

    Update TABUADA.cpp
    Nada além de uma tabuada normal. Sobre o código da mudança da cor do prompt de comando:
    system("color cor_do_fundo+cor _da_letra"); funciona com o sistema hexadecimal:
    
    0 = Preto ; 1 =Azul ; 2=Verde ; 3=Verde-Aguá ; 4=Vermelho ; 5= Roxa ; 6 = Amarelo ; 7=Branco ; 8= Cinza ; 9= Azul Claro  ;  A=Verde Claro ; B= Verde-água claro ; C= Vermelho-Claro ; D= Lilás ; E= Amarelo Claro ; F= Branco Brilhante

--- aula 07032023/TABUADA.cpp
+++ aula 07032023/TABUADA.cpp
@@ -6,14 +6,14 @@ main(){
 
 setlocale(LC_ALL, "Portuguese");
 
-system("color 78");
+system("color 78"); //muda a cor do promp de comando
 
 int num, cont=0; //iniciarei com a tabuada o 0
 
-while(cont<=10){ //enquanto a a condição verdadeira, faça
+while(cont<=10){ //enquanto a a condiÃ§Ã£o verdadeira, faÃ§a
 	num = 0; //atrinui o valor 0 a variavel num
 	
-	while (num<=10){ //enquanto a a condição verdadeira, faça
+	while (num<=10){ //enquanto a a condiÃ§Ã£o verdadeira, faÃ§a
 	
 
 	printf("\n %d X %d = %d \n", cont,num,cont*num);
