c22933a296e0a10aa2dccf3d5948d8c71671f912
Raposo <92121267+at-raposo@users.noreply.github.com>
2023-03-07 20:27:17 -0300

    Add files via upload

---
+++ aula 07032023/TABUADA.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+main(){
+
+setlocale(LC_ALL, "Portuguese");
+
+system("color 78");
+
+int num, cont=0; //iniciarei com a tabuada o 0
+
+while(cont<=10){ //enquanto a a condição verdadeira, faça
+	num = 0; //atrinui o valor 0 a variavel num
+	
+	while (num<=10){ //enquanto a a condição verdadeira, faça
+	
+
+	printf("\n %d X %d = %d \n", cont,num,cont*num);
+	num ++;
+}
+	printf("\n");
+	cont ++;
+}
+	
+	return 0;	
+	
+	
+	
+	
+	
+}
